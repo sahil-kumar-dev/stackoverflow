0e212af45ba2f05c65316ac463a7922f78c6c649
Sahil Kumar <sahilkumarofficial05@gmail.com>
2024-07-24 02:55:11 +0530

    added assets and changes layout

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,8 +1,9 @@
+import { SignUp } from '@clerk/nextjs'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>SignUp</div>
+    <SignUp/>
   )
 }
 
---
+++ middleware.ts
@@ -0,0 +1,9 @@
+import { clerkMiddleware,authMiddleware } from "@clerk/nextjs/server";
+
+export default clerkMiddleware({
+    
+});
+
+export const config = {
+    matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
